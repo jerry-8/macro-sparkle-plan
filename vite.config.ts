39ba8bcdbc3aae9275011bbcdce1bbeaabdc875f
jerry-8 <91873044+jerry-8@users.noreply.github.com>
2026-06-24 15:13:01 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@
 //     componentTagger (dev-only), VITE_* env injection, @ path alias, React/TanStack dedupe,
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
-/*
+
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 //import { nitro } from "nitro/vite";  // for vercel
 
 export default defineConfig({
+  base: '/jerry-8.github.io/',
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
@@ -24,8 +25,8 @@ export default defineConfig({
   //  ],
   // },
 });
-*/
 
+/*
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -46,4 +47,4 @@ export default defineConfig({
     outDir: 'dist',
   },
 })
-
+*/
