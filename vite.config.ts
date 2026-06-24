23ed9126cd78354e2e352f5d26b7d34888175336
jerry-8 <91873044+jerry-8@users.noreply.github.com>
2026-06-24 11:19:26 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-//import { nitro } from "nitro/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   tanstackStart: {
@@ -14,11 +14,11 @@ export default defineConfig({
     server: { entry: "server" },
   },
   plugins: [],
-  //vite: {
-    //plugins: [
-      //nitro({
-        //preset: "vercel",
-      //}),
-    //],
-  //},
+  vite: {
+    plugins: [
+      nitro({
+        preset: "vercel",
+      }),
+    ],
+  },
 });
